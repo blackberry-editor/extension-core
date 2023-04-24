79fc07f7fd02ece27478e387a00320b53604db65
frqstbite <40967034+frqstbite@users.noreply.github.com>
2023-04-24 01:52:49 +0000

    Extension structure prepared

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
-import { maps } from "@blackberryeditor/core"
-
-const node = new maps.Node(10, 150);
-console.log(typeof node);
-console.log(node);
+export default {
+    name: "Blackberry Core",
+    description: "Core, game-agnostic content that ships with Blackberry.",
+    version: "0.0.1",
+    author: {
+        "name": "Frqstbite",
+        "discord": "ashi.lied#0143"
+    },
+}
